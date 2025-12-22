1a47aca3da987021b17a639482020e3892b42d8a
Ron <137854483+Ronlam22@users.noreply.github.com>
2025-12-23 06:46:30 +0800

    Add files via upload

--- switch_nftable.sh
+++ switch_nftable.sh
@@ -65,6 +65,10 @@ echo "  TPROXY端口    : $TPROXY_PORT"
 echo "  fake_ipv4     : $FAKE_IPV4_CIDR"
 echo "  fake_ipv6     : $FAKE_IPV6_CIDR"
 echo
+SINGBOX_GID="$(ps -o gid= -C sing-box 2>/dev/null | head -n1 | tr -d ' ')"
+[[ -z "$SINGBOX_GID" ]] && \
+SINGBOX_GID="$(ps -o gid= -C singbox 2>/dev/null | head -n1 | tr -d ' ')"
+[[ -z "$SINGBOX_GID" ]] && SINGBOX_GID=0
 
 if [[ "$MODE" == "global" ]]; then
   # 全局模式(Global Mode)
@@ -125,6 +129,7 @@ chain redirect-prerouting {
 chain redirect-output {
     type nat hook output priority dstnat; policy accept;
     meta l4proto != tcp return
+    skgid $SINGBOX_GID return
     fib daddr type { unspec, local, anycast, multicast } return
     ip daddr @fake_ipv4 meta l4proto tcp redirect to :$REDIRECT_PORT
     ip6 daddr @fake_ipv6 meta l4proto tcp redirect to :$REDIRECT_PORT
@@ -146,6 +151,7 @@ chain tproxy-prerouting {
 chain tproxy-output {
     type filter hook output priority mangle; policy accept;
     meta l4proto != udp return
+    skgid $SINGBOX_GID return
     fib daddr type { unspec, local, anycast, multicast } return
     ip daddr @local_ipv4 return
     ip6 daddr @local_ipv6 return
@@ -218,6 +224,7 @@ chain tproxy-prerouting {
 chain tproxy-output {
     type filter hook output priority mangle; policy accept;
     meta l4proto != { tcp, udp } return
+    skgid $SINGBOX_GID return
     fib daddr type { unspec, local, anycast, multicast } return
     ip daddr @local_ipv4 return
     ip6 daddr @local_ipv6 return
@@ -293,6 +300,7 @@ chain redirect-prerouting {
 chain redirect-output {
     type nat hook output priority dstnat; policy accept;
     meta l4proto != tcp return
+    skgid $SINGBOX_GID return
     fib daddr type { unspec, local, anycast, multicast } return
     ip daddr @fake_ipv4 meta l4proto tcp redirect to :$REDIRECT_PORT
     ip6 daddr @fake_ipv6 meta l4proto tcp redirect to :$REDIRECT_PORT
@@ -317,6 +325,7 @@ chain tproxy-prerouting {
 chain tproxy-output {
     type route hook output priority mangle; policy accept;
     meta l4proto != udp return
+    skgid $SINGBOX_GID return
     ip daddr @fake_ipv4 meta mark set 1 accept
     ip6 daddr @fake_ipv6 meta mark set 1 accept
     ip daddr @telegram_ipv4 meta mark set 1 accept
@@ -393,6 +402,7 @@ chain tproxy-prerouting {
 chain tproxy-output {
     type route hook output priority mangle; policy accept;
     meta l4proto != { tcp, udp } return
+    skgid $SINGBOX_GID return
     ip daddr @fake_ipv4 meta mark set 1 accept
     ip6 daddr @fake_ipv6 meta mark set 1 accept
     ip daddr @telegram_ipv4 meta mark set 1 accept
